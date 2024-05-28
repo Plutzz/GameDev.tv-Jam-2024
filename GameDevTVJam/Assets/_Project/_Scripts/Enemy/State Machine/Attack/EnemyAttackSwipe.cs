1da0af458171b155453662b735ab75454d116229
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2024-05-27 21:31:15 -0700

    attack concept for reacher

---
+++ GameDevTVJam/Assets/_Project/_Scripts/Enemy/State Machine/Attack/EnemyAttackSwipe.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy States/Attack/Swipe")]
+public class EnemyAttackSwipe : EnemyAttackSOBase
+{
+    [SerializeField] private float attackChargeTime = 1f;
+    private float timer;
+
+    public override void DoEnterLogic()
+    {
+        base.DoEnterLogic();
+
+        timer = attackChargeTime;
+    }
+
+    public override void DoUpdateState()
+    {
+        base.DoUpdateState();
+
+        timer -= Time.deltaTime;
+        Debug.Log("attack charging");
+
+        if(timer < 0f)
+        {
+            Debug.Log("attacking");
+            //play swing animation
+            stateMachine.ChangeState(enemy.IdleState);
+        }
+    }
+
+    public override void DoExitLogic()
+    {
+        base.DoExitLogic();
+    }
+}
