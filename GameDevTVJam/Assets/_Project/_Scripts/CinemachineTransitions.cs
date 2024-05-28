163857e9e9b6f3477ad65b1fbc73d093e2cddf6d
Plutzz <benjiongco@gmail.com>
2024-05-28 12:19:19 -0700

    Setup tutorial scene

---
+++ GameDevTVJam/Assets/_Project/_Scripts/CinemachineTransitions.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinemachineTransitions : MonoBehaviour
+{
+    private Animator anim;
+    [SerializeField] private string stateName;
+    private void Awake()
+    {
+        anim = GetComponentInParent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Player Enter Camera Transition");
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            SwitchCamera(stateName);
+        }
+    }
+
+    private void SwitchCamera(string stateName)
+    {
+        StartCoroutine(FreezeGame(0.5f));
+        anim.Play(stateName);
+    }
+
+    private IEnumerator FreezeGame(float seconds)
+    {
+        Time.timeScale = 0;
+        yield return new WaitForSecondsRealtime(seconds);
+        Time.timeScale = 1;
+    }
+}
